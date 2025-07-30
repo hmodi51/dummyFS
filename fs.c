e25a92a49720fc2aea052f88cd649091809149a3
hmodi51 <modiharshit01@gmail.com>
2025-07-30 02:32:52 -0500

    filled in superblock objects and operations'

--- fs.c
+++ fs.c
@@ -6,20 +6,43 @@
 
 
 
-struct file_system_type dummyFS_type {
+struct file_system_type dummyFS_type = {
     .name = "dummyFS",
     .owner = THIS_MODULE,
     .mount = dummy_mount,
-    .kill_sb = dummyFS_kill,
+    .kill_sb = dummyFS_kill,    // unimplemented
     .next = NULL
 };
 
 
-struct dentry *mount(struct file_system_type *fs_type , int flags , const char *dev_name , void *data){
-    mount_nodev(fs_type , flags , data , dummy_fill_super);
+/* 
+implementation for mounting the file system
+*/
+struct dentry *dummy_mount(struct file_system_type *fs_type , int flags , const char *dev_name , void *data){
+   return mount_nodev(fs_type , flags , data , dummy_fill_super);
+}
+
+void dummyFS_kill(struct super_block *sb){
+    // implementation for unmounting the file system
 }
 
 
+struct super_operations dummyfs_super_ops = {
+    // have to fill struct of super operations
+    .put_super = dummy_put_super,        // unimplemented
+    .statfs = dummy_stat_fs,             // unimplemented
+    .alloc_inode = dummy_alloc_inode,    // unimplemented
+    .drop_inode = generic_delete_inode,  // already there in kernel files
+    .free_inode = dummy_free_inode
+};
+
+/*
+implementation for filling the superblock object
+*/
 static int dummy_fill_super(struct super_block *sb , void *data , int silent){
     sb->smagic = DUMMY_MAGIC;
+    sb->s_blocksize = BLOCKSIZE;
+    SB->s_blocksize_bits = BLOCKSIZEBITS;
+    sb->s_op = &dummyfs_super_ops;
 }
+
--- fs.h
+++ fs.h
@@ -2,5 +2,7 @@
 #define FS_H
 
 #define DUMMY_MAGIC 0x8BADF00D
+#define BLOCKSIZEBITS 12
+#define BLOCKSIZE (1 << BLOCKSIZEBITS)
 
 #endif
